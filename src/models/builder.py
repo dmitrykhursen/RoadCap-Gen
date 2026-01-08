b831b8caf15035663f61db79f2117ccb5c63686f
Dmytro Khursenko <dmitryxyrsenko@gmail.com>
2026-01-08 09:15:30 +0100

    Update Llava1.6 code

--- src/models/builder.py
+++ src/models/builder.py
@@ -1,19 +1,28 @@
 # src/models/builder.py
 from .llava_module import RoadCapLLaVA
 from transformers import AutoConfig
+import torch
 
 def build_model(cfg):
     print(f"Loading Model: {cfg.model.path}")
+
+    if getattr(cfg.training, "bf16", False):
+        compute_dtype = torch.bfloat16
+        print("Model loading with: bfloat16 (Best for A100)")
+    else:
+        compute_dtype = torch.float16
+        print("Model loading with: float16")
     
     if cfg.model.type == "llava":
-        # Load Config first
+        # load config first
         hf_config = AutoConfig.from_pretrained(cfg.model.path)
+        # print(f"HF Config Loaded: {hf_config}")
         
         # Load Model (handling 4-bit loading happens in peft_utils usually, 
         # but for simplicity we load full here, or let Trainer handle it)
         model = RoadCapLLaVA.from_pretrained(
             cfg.model.path,
-            torch_dtype="auto",
+            dtype=compute_dtype,
         )
         return model
     else:
--- src/models/llava_module.py
+++ src/models/llava_module.py
@@ -10,22 +10,20 @@ class RoadCapLLaVA(LlavaNextForConditionalGeneration):
         
         # Initialize Projection Head (Required for your future Extended Mode)
         # LLaVA 1.6 Vicuna usually has hidden size 4096
-        self.geo_projector = nn.Linear(4096, 256) 
+        # self.geo_projector = nn.Linear(4096, 256) 
 
     @classmethod
     def from_pretrained(cls, path, **kwargs):
         # Override to load correctly from HF
         model = super().from_pretrained(path, **kwargs)
-        # Manually initialize the projector since it's not in the pre-trained weights
-        model.geo_projector = nn.Linear(4096, 256)
-        return model
 
-    def get_lora_target_modules(self):
-        # LLaVA 1.6 uses these layers. Targeting 'vision_tower' is optional but often helps.
-        return ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"]
+        # # Manually initialize the projector since it's not in the pre-trained weights
+        # model.geo_projector = nn.Linear(4096, 256)
+        return model
 
     def get_visual_features(self, outputs):
         # Extract features for Extended Mode
         # LLaVA-NeXT (1.6) outputs can vary, but usually:
         hidden_states = outputs.hidden_states[-1]
-        return self.geo_projector(hidden_states)
+        # hidden_states = self.geo_projector(hidden_states)
+        return hidden_states
