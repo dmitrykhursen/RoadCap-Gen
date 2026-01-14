5065ced15613ea77f6d0212f240d52792f88eebb
l-Dmytro KHURSENKO <l-dkhursen@pra1-w62057.vnet.valeo.com>
2026-01-14 16:51:58 +0100

    First sucessful train & eval train run localy

--- scripts/02_finetuning/train.py
+++ scripts/02_finetuning/train.py
@@ -2,132 +2,88 @@ import hydra
 import torch
 from transformers import TrainingArguments
 from src.models.builder import build_model
+from src.training import trainer
 from src.training.peft_utils import apply_peft
 from src.training.trainer import RoadCapTrainer
 from src.data.dataset import RoadCapDataset
 from src.data.collator import RoadCapCollator
-from transformers import AutoTokenizer, AutoImageProcessor
+from transformers import AutoTokenizer, AutoImageProcessor, AutoProcessor
 
-# def test_single_batch(dataset):
-#     from torch.utils.data import DataLoader
+import sys
+import logging
+import transformers
+import wandb
+from omegaconf import OmegaConf
 
-#     print(f"\n🔍 DEBUGGING DATASET...")
-#     print(f"Dataset type: {type(dataset.flat_samples)}")
-    
-#     # 1. Test direct access (The exact line that crashed before)
-#     try:
-#         print("Attempting dataset[0]...")
-#         item = dataset[0]
-#         print("✅ dataset[0] success!")
-#         # Optional: Print keys to verify structure
-#         # print(f"Sample keys: {item.keys()}") 
-#     except KeyError as e:
-#         print(f"❌ CRASH: dataset[0] failed with KeyError: {e}")
-#         print(f"Reason: self.data is a Dictionary, but we tried to access index 0.")
-#         return
+# setup Python's native logging to print to stdout (Terminal)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO, # Force native logger to INFO
+)
 
-#     except Exception as e:
-#         print(f"❌ CRASH: {e}")
-#         return
-    
-#     # Initialize it with the tokenizer from the dataset
-#     collator = RoadCapCollator(tokenizer=dataset.tokenizer)
-    
-
-#     # 2. Test DataLoader (what the Trainer does)
-#     print("\nAttempting DataLoader next(iter())...")
-#     loader = DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=collator)
-
-    
-#     try:
-#         batch = next(iter(loader))
-#         print("✅ DataLoader success!")
-#         print(f"Batch keys: {batch.keys()}")
-#         if 'input_ids' in batch:
-#             print(f"Input shape: {batch['input_ids'].shape}")
-#     except Exception as e:
-#         print(f"❌ DataLoader failed: {e}")
+# force Hugging Face Transformers to print INFO logs
+transformers.utils.logging.set_verbosity_info()
+# only print on the main GPU (Rank 0) to avoid duplicates in distributed training
+transformers.utils.logging.enable_default_handler()
+transformers.utils.logging.enable_explicit_format()
 
 
 @hydra.main(version_base=None, config_path="../../configs", config_name="config")
 def main(cfg):
+    print("=== RoadCap-Gen Fine-tuning Script ===")
+    print(f"Args / config: {cfg}")
+
+    # convert Hydra config to  dict
+    config_dict = OmegaConf.to_container(cfg, resolve=True)
+    # initi WANDB
+    wandb_run = wandb.init(
+        project="RoadCap-Gen",      # Your Project Name
+        name=cfg.experiment_name,   # Name of this specific run (e.g. "qa_test_v1")
+        config=config_dict,         # <--- THIS LOGS YOUR HYDRA CONFIG
+        group=cfg.model.name,       # Optional: Group runs by model type
+        job_type="finetune",
+    )
+
     # ---------------------------------------------------------
-    # 1. LOAD TOKENIZER FIRST
-    # ---------------------------------------------------------
+    # 1. load Tokenizer & Processor
     print(f"Loading generic tokenizer and processor from {cfg.model.path}...")
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.path, use_fast=False, trust_remote_code=True)
     
     # Fix Pad Token
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.unk_token if tokenizer.unk_token else tokenizer.eos_token
+    
+    image_processor = AutoImageProcessor.from_pretrained(cfg.model.path, trust_remote_code=True)
+    # print("image_processor:", image_processor)
 
-    # ---------------------------------------------------------
-    # 2. BUILD MODEL (DO THIS ONLY ONCE)
-    # ---------------------------------------------------------
+    # 2. Build Model
     print(f"Building Model: {cfg.model.name}")
     model = build_model(cfg)  
-    # #####
-    # # model.config.image_grid_pinpoints = None 
-    # model.config.image_grid_pinpoints = [[336, 336]]
-    # model.config.vision_feature_select_strategy = "default"
-    # model.config.image_aspect_ratio = "square"
-    # if hasattr(model.config.vision_config, "patch_size"):
-    #     model.config.vision_config.patch_size = 14
-    # #####
-
-    # ---------------------------------------------------------
-    # 3. FIX TOKENIZER & MODEL CONFIG (SYNC THEM)
-    # ---------------------------------------------------------
-    image_token = "<image>"
-    token_ids = tokenizer.encode(image_token, add_special_tokens=False)
-    
-    # A. Ensure <image> is a single token
-    if len(token_ids) > 1 or image_token not in tokenizer.get_vocab():
-        print(f"⚠️ Tokenizer is splitting '{image_token}'! Fixing it now...")
-        tokenizer.add_tokens([image_token], special_tokens=True)
         
-        # B. RESIZE MODEL EMBEDDINGS (Critical step applied to the loaded model)
-        model.resize_token_embeddings(len(tokenizer))
-        print(f"✅ Added <image> token and resized model embeddings.")
-    else:
-        print(f"✅ Tokenizer correctly recognizes <image>")
-
-    # C. SYNC CONFIG (Critical step applied to the loaded model)
-    model.config.image_token_index = tokenizer.convert_tokens_to_ids("<image>")
-    model.config.pad_token_id = tokenizer.pad_token_id
-    print(f"⚙️ Synced Config: image_token_index={model.config.image_token_index}")
-
-    # ---------------------------------------------------------
-    # 4. LOAD DATASET
-    # ---------------------------------------------------------
-    # image_processor = AutoImageProcessor.from_pretrained(
-    #             cfg.model.path,
-    #             do_resize=True,
-    #             size={"shortest_edge": 336},
-    #             # size={"shortest_edge": 224},
-    #             do_center_crop=False,
-    #             do_rescale=True,
-    #             # crop_size={"height": 336, "width": 336},
-    #             rescale_factor=1 / 255,
-    #             do_normalize=True,
-    #             image_mean=[0.48145466, 0.4578275, 0.40821073], # this is based on imagenet !!! so compute based on our training data
-    #             image_std=[0.26862954, 0.26130258, 0.27577711], # this is based on imagenet !!! so compute based on our training data
-    #             # image_grid_pinpoints=[[336, 1008], [1008, 336]],
-    #             do_pad=True,
-    #             do_convert_rgb=True
-    #         )
-        
-    image_processor = AutoImageProcessor.from_pretrained(cfg.model.path, trust_remote_code=True)
-    print("image_processor:", image_processor)
-    # exit(0)
     
-    print(f"Loading Dataset from {cfg.dataset.data_path}")
-    dataset = RoadCapDataset(
+    print(f"Loading Datasets from {cfg.dataset.data_path}...")
+    # create Training Dataset (e.g., 10% of Train Split)
+    print("--- Loading Training Set ---")
+    train_dataset = RoadCapDataset(
         data_path=cfg.dataset.data_path,
         image_folder=cfg.dataset.image_folder,
         tokenizer=tokenizer,
         image_processor=image_processor,
-        model_style="llava"
+        split="train",       # <--- Request Train split
+        data_usage=0.01      # <--- Train on only XX% of the training data
+    )
+
+    # create Validation Dataset (100% of Val Split)
+    print("--- Loading Validation Set ---")
+    eval_dataset = RoadCapDataset(
+        data_path=cfg.dataset.data_path,
+        image_folder=cfg.dataset.image_folder,
+        tokenizer=tokenizer,
+        image_processor=image_processor,
+        split="val",         # <--- Request Validation split
+        data_usage=0.4       # <--- Use XX% of validation data
     )
 
     # 5. Apply LoRA
@@ -135,34 +91,69 @@ def main(cfg):
 
     # 6. Training Arguments & Trainer
     use_bf16 = getattr(cfg.training, "bf16", False)
-    
+
+
     training_args = TrainingArguments(
         output_dir=f"output/{cfg.model.name}/{cfg.experiment_name}",
+        report_to="wandb",          # log to W&B
+        run_name=cfg.experiment_name,
         per_device_train_batch_size=cfg.training.batch_size,
         gradient_accumulation_steps=cfg.training.grad_accumulation,
         learning_rate=cfg.training.learning_rate,
         num_train_epochs=cfg.training.epochs,
         logging_steps=cfg.training.logging_steps,
-        save_strategy="epoch",
+        logging_strategy="epoch", # or "steps"
+        # save_strategy="epoch",
         bf16=use_bf16,
         fp16=not use_bf16,
-        dataloader_num_workers=1,
-        remove_unused_columns=False
+        dataloader_num_workers=cfg.training.num_workers,
+        remove_unused_columns=False, 
+        gradient_checkpointing=cfg.training.grad_checkpointing,
+
+        # evaluation / validation
+        # evaluation_strategy="epoch", # Run validation at end of every epoch
+        per_device_eval_batch_size=cfg.training.batch_size * 2, # usually can be larger for eval because I don't need gradients
+        eval_strategy="epoch",
+        save_strategy="epoch",
+
+        save_total_limit=2, # keep only last 2 checkpoints, saves space
+        # ENABLE "BEST" SAVING and save_total_limit=2 means saving 1 Best + 1 Latest ckpts
+        load_best_model_at_end=True,  # Automatically load the best model when training finishes
+        # DEFINE "BEST"
+        metric_for_best_model="eval_loss", # or "accuracy" if you calculate it
+        greater_is_better=False,           # False for loss (lower is better), True for accuracy
+
+        # # Reporting
+        # report_to="wandb",            
+        # run_name="llava-finetune-v1"
+
     )
+    # collator = RoadCapCollator(tokenizer=tokenizer)
+
     
-    collator = RoadCapCollator(tokenizer=tokenizer)
+
+    processor = AutoProcessor.from_pretrained(cfg.model.path)
+    processor.tokenizer.padding_side = "right" # during training, one always uses padding on the right
+    collator = RoadCapCollator(processor=processor)
     
     trainer = RoadCapTrainer(
         model=model,
         args=training_args,
-        train_dataset=dataset,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
         data_collator=collator,
         mode="simple"
     )
-    
+
+    print("📊 Running Epoch 0 (Baseline) Evaluation / Validation...")
+    initial_metrics = trainer.evaluate()
+    initial_metrics["epoch"] = 0. # force 'epoch' to be 0.0
+    trainer.log(initial_metrics) # log to W&B (and console)
+    print(f"Epoch 0 Metrics: {initial_metrics}")
+
+
     print("Starting Training...")
     trainer.train()
-
 
     print("Finished.")
 
