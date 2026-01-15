55a287bbccb863787a3969f137633604b3017a7a
Dmytro Khursenko <dmitryxyrsenko@gmail.com>
2026-01-15 10:48:54 +0100

    Add karolina srun script

---
+++ scripts/02_finetuning/karolina_train.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=finetune_llava_valeo
+#SBATCH --account=EU-25-10
+#SBATCH --time=40:00:00 # Time limit for running
+#SBATCH --partition=qgpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=dmytro.khursenko@valeo.com
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+# Load necessary modules (if applicable)	
+source /mnt/proj1/eu-25-10/envs/roadcap-gen/bin/activate
+cd /mnt/proj1/eu-25-10/dmytro/RoadCap-Gen/
+
+# Define variables
+MODEL="llava"
+DATASET="vqa_valeo_karolina"
+TRAINING="lora"
+
+experiment_name="karolina_srun_llava_fullfinetune_MMP_Valeo_3.1"
+
+# Run the command
+srun python scripts/02_finetuning/train.py \
+    model=$MODEL     \
+    dataset=$DATASET     \
+    training=$TRAINING     \
+    experiment_name=$experiment_name     \
+
+
+
+# sbatch scripts/02_finetuning/karolina_train.sh
