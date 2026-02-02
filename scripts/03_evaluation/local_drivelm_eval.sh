b6d8e8089f09ba3666bf78e5993407a23d6d0588
Dmytro Khursenko <dmitryxyrsenko@gmail.com>
2026-02-02 13:43:13 +0100

    Add template scripts (tbf later) to pseudolabels gen and evaluation

---
+++ scripts/03_evaluation/local_drivelm_eval.sh
@@ -0,0 +1,3 @@
+source ../../envs/drivelm/bin/activate && ml Java && python external/DriveLM/challenge/evaluation.py --root_path1="output/inference/tmp_debug_infer_llava_drivelm_local_val_2026-01-22_19-28-48/drivelm_pred_forHF_2026-01-22_19-28-48.json" --root_path2="/mnt/proj1/eu-25-10/dmytro/RoadCap-Gen/external/DriveLM/challenge/data/v1_1_train_converted.json"
+
+# bash scripts/03_evaluation/local_drivelm_eval.sh
